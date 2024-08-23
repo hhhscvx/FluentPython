bb94bbd26486f796694643a170a7facbb01ee81d
hhhscvx <hhhscvx@protonmail.com>
2024-08-23 11:46:21 +0500

    chapter1

---
+++ chapter1/main.py
@@ -0,0 +1,35 @@
+from typing import NamedTuple
+
+
+class Card(NamedTuple):
+    rank: str
+    suit: str
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self) -> None:
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+deck = FrenchDeck()
+print(deck.ranks)
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card: Card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * 4 + suit_values[card.suit]
+
+
+for card in sorted(deck, key=spades_high):
+    ...
---
+++ chapter1/vector.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import math
+
+
+class Vector:
+    def __init__(self, x=0, y=0) -> None:
+        self.x = x
+        self.y = y
+
+    def __add__(self, other: Vector) -> Vector:
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar: int) -> Vector:
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __repr__(self) -> str:
+        return f'Vector({self.x!r}, {self.y!r})'
+
+
+v1 = Vector(1, 3)
+v2 = Vector(4, 2)
+
+print(v1 * 2)
